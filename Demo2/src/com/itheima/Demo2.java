a1fa875600391c9dc34142d38c0b81c1fc8229c9
Administrator <Administrator@2013-20150211AW>
2017-11-11 22:03:08 +0800

    111

--- Demo2/src/com/itheima/Demo2.java
+++ Demo2/src/com/itheima/Demo2.java
@@ -1,7 +1,7 @@
 package com.itheima;
 
-public class demo1 {
+public class Demo2 {
 	public static void main(String[] args) {
-		System.out.println("demo1");
+		System.out.println("demo2");
 	}
-}	
+}
